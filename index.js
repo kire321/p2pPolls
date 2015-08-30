215a1cefaac0dcd3b44542cca8c80b729bb4d4e6
Erik Wyatt <kire321@gmail.com>
2015-08-30 17:20:19 +0200

    peerjs hello world

--- index.js
+++ index.js
@@ -5,30 +5,37 @@ var _ = require('lodash')
 var myId = _.random(0, 99)
 console.log("myId", myId)
 
-function handleNewConnection(conn) {
+function handleNewConnection(conn, source) {
     conn.on('data', function(data) {
+        console.log("got data")
         console.log(data)
     })
     conn.on('error', function(err) {
         console.log(err)
     })
-    setInterval(function() {
-        console.log("sending...")
+    // var interval = setInterval(function() {
+        console.log("sending", source, conn.dataChannel.readyState, conn.dataChannel.bufferedAmount)
         conn.send("hello")
-    }, 1000)
+    // }, 1000)
+    conn.on('close', function() {
+        clearInterval(interval)
+    })
 }
 
 var peer = new Peer(myId, {key: 'qdpeahtrzay06bt9'})
 peer.on('connection', function(conn) {
     console.log("got a connection")
-    handleNewConnection(conn)
+    conn.on('open', function() {
+        console.log("inbound connection is open")
+        handleNewConnection(conn, "inbound")
+    })
 })
 
 function connect(id) {
     return new Promise(function(resolve, reject) {
         var conn = peer.connect(id)
-        conn.on('error', function() {
-            resolve(null)
+        conn.on('error', function(err) {
+            reject(err)
         })
         conn.on('open', function() {
             console.log("connection succeeded", id)
@@ -38,13 +45,10 @@ function connect(id) {
 }
 
 var connections = _.range(100).map(connect)
-var successfulConnections = Promise.filter(connections, function (conn) {
-    conn !== null
-})
-var firstPeerPromise = Promise.any(successfulConnections)
+var firstPeerPromise = Promise.any(connections)
 firstPeerPromise.then(function (firstPeer) {
     console.log("i have friends!!")
-    handleNewConnection(firstPeer)
+    handleNewConnection(firstPeer, "outbound")
 })
 
 document.body.textContent = "Hello World!"
