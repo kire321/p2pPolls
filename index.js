4c9c7ed197ae2f98f6723f8d90986baed99d37e9
Erik Wyatt <kire321@gmail.com>
2015-08-30 15:35:49 +0200

    peerjs send msg try 1

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 var Peer = require('peerjs')
+var Promise = require('bluebird')
+var _ = require('lodash')
 
-var peer = new Peer(0, {key: 'qdpeahtrzay06bt9', debug: 3})
-peer.on('error', function(err) {
-    console.log(err)
+var myId = _.random(0, 99)
+console.log("myId", myId)
+
+function handleNewConnection(conn) {
+    conn.on('data', function(data) {
+        console.log(data)
+    })
+    conn.on('error', function(err) {
+        console.log(err)
+    })
+    setInterval(function() {
+        console.log("sending...")
+        conn.send("hello")
+    }, 1000)
+}
+
+var peer = new Peer(myId, {key: 'qdpeahtrzay06bt9'})
+peer.on('connection', function(conn) {
+    console.log("got a connection")
+    handleNewConnection(conn)
 })
-var conn = peer.connect(1)
-conn.on('error', function(err) {
-    console.log(err)
+
+function connect(id) {
+    return new Promise(function(resolve, reject) {
+        var conn = peer.connect(id)
+        conn.on('error', function() {
+            resolve(null)
+        })
+        conn.on('open', function() {
+            console.log("connection succeeded", id)
+            resolve(conn)
+        })
+    })
+}
+
+var connections = _.range(100).map(connect)
+var successfulConnections = Promise.filter(connections, function (conn) {
+    conn !== null
 })
-conn.on('data', function(data) {
-    console.log(data)
-})
-conn.on('open', function() {
-    conn.send("hello")
+var firstPeerPromise = Promise.any(successfulConnections)
+firstPeerPromise.then(function (firstPeer) {
+    console.log("i have friends!!")
+    handleNewConnection(firstPeer)
 })
 
 document.body.textContent = "Hello World!"
